524732031f81d0ceb43c8116c67f5d389b3a99e3
To <theogrillon6f9@gmail.com>
2023-03-28 13:01:34 +0200

    New file to the new branch

---
+++ anotherfile.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void print(int a) {
+    std::cout << "this is an integer value : " << a << std::endl ;
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include "anotherfile.cpp"
+
+int mult(int a, int b) {
+    return a * b ;
+}
+
+int add(int a, int b) {
+    return a + b ;
+}
 
 int main(int argc, char** argv) {
-    int a = 0 ;
+    int a = 4 ;
     int b = 5 ;
-
-    std::cout << a + b << std::endl ;
+    int c = 6 ;
+    std::cout << "a + b = " << add(a, b) << std::endl ;
+    std::cout << "a * b = " << mult(a, b) << std::endl ;
     std::cout << "Hello world!" << std::endl ;
+    print(c) ;
     return 0 ;
 }
