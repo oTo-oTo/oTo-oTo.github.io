68798f2a081f197830b2032b406f6140d012f109
To <theogrillon6f9@gmail.com>
2023-03-28 14:42:25 +0200

    adding sum and mult functions to main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include "anotherfile.cpp"
 
+int mult(int a, int b) {
+    return a * b ;
+}
+
+int sum(int a, int b) {
+    return a + b ;
+}
+
 int main(int argc, char** argv) {
     int a = 4 ;
     int b = 5 ;
     int c = 6 ;
 
-    std::cout << a + b << std::endl ;
+    std::cout << "a * b = " << mult(a, b) << std::endl ;
+    std::cout << "a + b = " << sum(a, b) << std::endl ;
     std::cout << "Hello world!" << std::endl ;
     print(c) ;
     return 0 ;
