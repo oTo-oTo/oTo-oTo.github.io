a758391db821f83c34417f968b270d11281f39bb
To <theogrillon6f9@gmail.com>
2023-03-28 12:37:29 +0200

    Modification of the .cpp file.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
+    int a = 0 ;
+    int b = 5 ;
 
+    std::cout << a + b << std::endl ;
     std::cout << "Hello world!" << std::endl ;
     return 0 ;
 }
