466642ae3f48865a536845ab121d4ed8dcf84d59
To <theogrillon6f9@gmail.com>
2023-03-28 13:07:28 +0200

    The min error as been fixed

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
+int min(int a, int b) { return (a < b) ? a : b ; }
+
 int main(int argc, char** argv) {
     int a = 0 ;
     int b = 5 ;
+
+    int c = min(a, b) ;
 
     std::cout << a + b << std::endl ;
     std::cout << "Hello world!" << std::endl ;
