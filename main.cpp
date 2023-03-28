9d80e2628b812e07fd45cc271556e2f5f85e5cae
To <theogrillon6f9@gmail.com>
2023-03-28 14:18:30 +0200

    Update .cpp file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "anotherfile.cpp"
 
 int main(int argc, char** argv) {
     int a = 4 ;
     int b = 5 ;
+    int c = 6 ;
 
     std::cout << a + b << std::endl ;
     std::cout << "Hello world!" << std::endl ;
